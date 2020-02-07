d0ccf4914aed93432965763c1384759be4fdd8d9
Ruth Pottgen <ruth.poettgen@cern.ch>
2020-02-07 13:27:25 +0000

    Merge branch 'IDPVM_21.0_fix_missing_tool_configuration_method' into '21.0'
    Added tool configuration method missing from recent master backport.
    
    See merge request atlas/athena!30028
    
    (cherry picked from commit b0e0071511054e24baccbc82400b829e80258e1c)
    
    87d052a1 Added tool configuration method missing from recent master backport.

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValDecoration.py
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValDecoration.py
@@ -120,6 +120,24 @@ def getInDetPhysHitDecoratorAlg(**kwargs) :
                                          ResidualPullCalculator = 'Trk::ResidualPullCalculator/ResidualPullCalculator',
                                          TrackParticleContainerName = 'InDetTrackParticles')
 
+def getInDetRttTruthSelectionTool(**kwargs) :
+    # should match truth selection of truth decorator
+    from AthenaCommon.AppMgr import ToolSvc
+    the_name='AthTruthSelectionTool'
+    if hasattr(ToolSvc,the_name) :
+        return getattr(ToolSvc,the_name)
+    from InDetPhysValMonitoring.InDetPhysValMonitoringConf import AthTruthSelectionTool
+    comb_kwargs={}
+    comb_kwargs.update(requireStatus1 = True,
+                      requireCharged = True,
+                      maxBarcode = ( 200*1000 if kwargs.pop("OnlyDressPrimaryTracks",True) else 2**31-1 ),
+                      maxProdVertRadius = 110.,
+                      maxEta = 2.5,
+                      minPt = 400. )
+    comb_kwargs.update(kwargs)
+    tool=AthTruthSelectionTool(**comb_kwargs)
+    ToolSvc += tool
+    return tool
 
 def getParameterErrDecoratorAlg(**kwargs) :
     '''
